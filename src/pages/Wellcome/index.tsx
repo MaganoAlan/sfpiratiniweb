3c212498b82bef1df673942b6fe29b14415f649b
Alan Magano <maganoalan@gmail.com>
2024-10-15 12:14:27 -0300

    verificação de matrícula duplicada

--- src/pages/Wellcome/index.tsx
+++ src/pages/Wellcome/index.tsx
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getAuth, updateProfile } from "firebase/auth";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 import Swal from "sweetalert2";
 import { Container, Image } from "./styles";
 import Logo from "../../assets/favicon.png";
@@ -17,12 +24,30 @@ export function Wellcome() {
   const [uid, setUid] = useState<any>("");
   const [newUser, setNewUser] = useState("");
   const [newMat, setNewMat] = useState("");
+  const [hasMat, setHasMat] = useState(false);
 
   useEffect(() => {
     setUser(auth.currentUser?.displayName);
     setEmail(auth.currentUser?.email);
     setUid(auth.currentUser?.uid);
   }, []);
+
+  async function studentVerify() {
+    const saturdayRef = collection(firestore, "alunos");
+    const q = query(saturdayRef, where("matricula", "==", newMat));
+    let response: any = [];
+    const res = await getDocs(q);
+    res.forEach((doc) => {
+      const data = doc.data();
+      response.push(data);
+    });
+    console.log("response", response);
+    if (response.length > 0) {
+      return setHasMat(true);
+    }
+    handleStudent();
+    navigate("/home");
+  }
 
   async function handleStudent() {
     if (user === null) {
@@ -47,6 +72,12 @@ export function Wellcome() {
     }
     return;
   }
+
+  useEffect(() => {
+    if (newMat.length > 3) {
+      studentVerify();
+    }
+  }, [newMat]);
 
   useEffect(() => {
     if (user === null) {
@@ -82,6 +113,25 @@ export function Wellcome() {
     }
   }, [user]);
 
+  useEffect(() => {
+    if (hasMat) {
+      Swal.fire({
+        title: "A matrícula informada já foi cadastrada!",
+        text: "Contate o administrador",
+        inputAttributes: {
+          autocapitalize: "off",
+        },
+        showCancelButton: false,
+        confirmButtonText: "Entendi",
+        showLoaderOnConfirm: true,
+        preConfirm: (user) => {
+          //setNewUser(user);
+        },
+        allowOutsideClick: () => !Swal.isLoading(),
+      });
+    }
+  }, [hasMat]);
+
   return (
     <Container>
       <Image src={Logo} alt="logo" />
@@ -90,8 +140,23 @@ export function Wellcome() {
         <DefaultButton
           title="BORA!"
           onClick={() => {
-            handleStudent();
-            navigate("/home");
+            studentVerify();
+            if (hasMat) {
+              Swal.fire({
+                title: "A matrícula informada já foi cadastrada!",
+                text: "Contate o administrador",
+                inputAttributes: {
+                  autocapitalize: "off",
+                },
+                showCancelButton: false,
+                confirmButtonText: "Entendi",
+                showLoaderOnConfirm: true,
+                preConfirm: (user) => {
+                  //setNewUser(user);
+                },
+                allowOutsideClick: () => !Swal.isLoading(),
+              });
+            }
           }}
         />
       </div>
