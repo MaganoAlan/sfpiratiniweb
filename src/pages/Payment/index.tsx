7e1c0bfd4fd9be9fb4d40293036f6299c572e093
Alan Magano <maganoalan@gmail.com>
2025-06-25 14:09:55 -0300

    tela pagamento pix

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -3,15 +3,19 @@ import { Container } from "../Home/styles";
 import { BtnContainer, Img, PixInput } from "./styles";
 import pixImage from "../../assets/pix.png"; // Adjust the path as necessary
 import DefaultButton from "../../components/DefaultButton";
+import Swal from "sweetalert2";
 
 export function Payment() {
   function copiarPix(text: string) {
     navigator.clipboard
       .writeText(text)
       .then(() => {
-        swal(
-          "Você copiou o PIX com sucesso! Agora é só colar no app do seu banco e pagar utilizando a chave CNPJ."
-        );
+        Swal.fire({
+          title: "Sucesso!",
+          text: "Você copiou o PIX com sucesso! Agora é só colar no app do seu banco e pagar utilizando a chave CNPJ.",
+          icon: "success",
+          confirmButtonText: "OK",
+        });
       })
       .catch((err) => {
         console.error("Erro ao copiar o PIX: ", err);
