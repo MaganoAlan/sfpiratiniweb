963c885b18c8aca8b22a8a5276d2adb5e61667c9
Alan Magano <maganoalan@gmail.com>
2026-02-27 20:15:46 -0300

    novas fontes

--- src/style/global.ts
+++ src/style/global.ts
@@ -1,6 +1,14 @@
 import { createGlobalStyle } from "styled-components";
-
+import VarelaRound from "../assets/fonts/Varela_Round/VarelaRound-Regular.ttf";
+// import Roboto from "../assets/fonts/Roboto-Regular.woff2";
 export const GlobalStyle = createGlobalStyle`
+   @font-face {
+      font-family: 'Varela Round';
+      src: url(${VarelaRound}) format('woff2');
+      font-weight: normal;
+      font-style: normal;
+      font-display: swap; /* Boa prática para performance */
+    }
   * {
     margin: 0;
     padding: 0;
