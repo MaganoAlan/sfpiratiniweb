ae383e9d6342b6a328d4a4fc8aef2d42498d13c9
Alan Magano <maganoalan@gmail.com>
2026-03-19 21:49:59 -0300

    ajuste tela home

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -10,7 +10,7 @@ export const DefaultBody = styled.div`
 
 export const Container = styled.div`
   //width: 100%;
-  height: 100vh;
+  height: 98vh;
   margin: auto;
   display: flex;
   flex-direction: column;
